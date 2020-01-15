0b5dc44f2a999a4837d062df14607771cb0ccb5e
sarah wang <sarahwang47@gmail.com>
2020-01-15 16:34:32 +0800

    react flat done

--- src/components/flatlist.jsx
+++ src/components/flatlist.jsx
@@ -6,10 +6,9 @@ class FlatList extends Component {
   render() {
     return (
     <div className="flat-list">
-      {this.props.flats.map(flat => {
+      {this.props.flats.map(flat =>
         <Flat key={flat.name} flat={flat} />
-      })
-    }
+      )}
     </div>
     );
   }
